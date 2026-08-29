3e6a340c81b992c300d8375d3b97228ed25a03a5
cutieshiba <htyantonia@gmail.com>
2026-08-29 22:59:34 +0800

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { GoogleOAuthProvider } from '@react-oauth/google'
+import App from './App.jsx'
+import './index.css'
+
+const CLIENT_ID = "629841934924-6eq6crc2flrtuvmqn4v71elqlntsb4el.apps.googleusercontent.com";
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <GoogleOAuthProvider clientId={CLIENT_ID}>
+      <App />
+    </GoogleOAuthProvider>
+  </React.StrictMode>,
+)
