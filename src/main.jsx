21650f54e9eaf21e79928adcf19b263dcc96a0cf
cutieshiba <htyantonia@gmail.com>
2026-08-29 23:21:20 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google'
 import App from './App.jsx'
 import './index.css'
 
-const CLIENT_ID = "629841934924-6eq6crc2flrtuvmqn4v71elqlntsb4el.apps.googleusercontent.com";
+const CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID || "629841934924-6eq6crc2flrtuvmqn4v71elqlntsb4el.apps.googleusercontent.com";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
